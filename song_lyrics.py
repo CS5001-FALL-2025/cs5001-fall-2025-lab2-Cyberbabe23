8b61dbc11f903f027809724b1d2f20957055c04d
Cyberbabe23 <mercyakinwumi36@gmail.com>
2025-10-31 18:52:38 -0500

    finished the print statement for song lyrics

--- song_lyrics.py
+++ song_lyrics.py
@@ -12,7 +12,19 @@ lyric on one single line using print.
 '''
 
 def main():
-    # YOUR CODE HERE
+    word1 = "You"
+    word2 = "are"
+    word3 = "worthy"
+    word4 = "to"
+    word5 = "be"
+    word6 = "Praised"
+    word7 = "King"
+    word8 = "of"
+    word9 = "Kings"
+    word10 = "Amen"
+    
+
+    print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10 + ".")
 
 
 if __name__ == '__main__':
